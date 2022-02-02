d85e34d46ba86fad79231f24a9988983c1fedb87
camila <ccamanio@dc.uba.ar>
2022-02-02 12:44:09 -0300

    add redis

---
+++ src/main/java/com/coderhouse/usuarios/handle/ApiRestException.java
@@ -0,0 +1,13 @@
+package com.coderhouse.usuarios.handle;
+
+import lombok.*;
+
+@Builder
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor(staticName = "of")
+public class ApiRestException extends Exception {
+    private String code;
+    private String message;
+}
---
+++ src/main/java/com/coderhouse/usuarios/utils/Constants.java
@@ -0,0 +1,6 @@
+package com.coderhouse.usuarios.utils;
+
+public class Constants {
+
+    public static final String NAME_MAP_MESSAGE = "mensaje-map";
+}
